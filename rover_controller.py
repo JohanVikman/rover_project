daf70be7202686b3df112053605f79e7513dbe78
JohanVikman <jowi7613@gmail.com>
2016-11-06 14:28:32 +0100

    Added controller which runs the motors using sixaxis controller

---
+++ rover_controller.py
@@ -0,0 +1,121 @@
+"""We want to control some engines using a sixaxis PS3 controller
+"""
+
+from pygame import joystick, event, display
+import RPi.GPIO as GPIO
+from time import sleep
+import pygame
+import os
+GPIO.setmode(GPIO.BOARD)
+
+# GPIO Pin motor mapping
+Motor1A = 16
+Motor1B = 18
+Motor1E = 22
+
+Motor2A = 23
+Motor2B = 21
+Motor2E = 19
+
+# PS 3 Button mapping:
+UP=4
+LEFT=7
+RIGHT=5
+DOWN=6
+TRI=12
+SQU=15
+CIR=0
+CRO=14
+SEL=0
+STA=3
+PS3=16
+LJO=1
+RJO=2
+L2=8
+L1=10
+R2=9
+R1=11
+
+def handle_button(event):
+    if event.type == pygame.JOYBUTTONDOWN:
+        if event.button == UP:
+            print "Going forwards"
+            GPIO.output(Motor1A,GPIO.HIGH)
+            GPIO.output(Motor1B,GPIO.LOW)
+            GPIO.output(Motor1E,GPIO.HIGH)
+
+        elif event.button == DOWN:
+            print "Going backwards"
+            GPIO.output(Motor1A,GPIO.LOW)
+            GPIO.output(Motor1B,GPIO.HIGH)
+            GPIO.output(Motor1E,GPIO.HIGH)
+        elif event.button == RIGHT:
+            print "Going right"
+            GPIO.output(Motor2A,GPIO.HIGH)
+            GPIO.output(Motor2B,GPIO.LOW)
+            GPIO.output(Motor2E,GPIO.HIGH)
+        elif event.button == LEFT:
+            print "Going left"
+            GPIO.output(Motor2A,GPIO.LOW)
+            GPIO.output(Motor2B,GPIO.HIGH)
+            GPIO.output(Motor2E,GPIO.HIGH)
+    elif event.type == pygame.JOYBUTTONUP:
+        print "Now stop"
+        if event.button == UP or event.button == DOWN:
+            GPIO.output(Motor1E,GPIO.LOW)
+        if event.button == RIGHT or event.button == LEFT:
+            GPIO.output(Motor2E,GPIO.LOW)
+        
+
+os.environ["SDL_VIDEODRIVER"]="dummy"
+if 1:
+    #some platforms might need to init the display for some parts of pygame.
+    import pygame.display
+    screen = pygame.display.set_mode((1,1))
+
+
+
+## Motor1 = Forwards/Backwards
+## Motor2 = Right/Left
+def setup_motor():
+    GPIO.setup(Motor1A,GPIO.OUT)
+    GPIO.setup(Motor1B,GPIO.OUT)
+    GPIO.setup(Motor1E,GPIO.OUT)
+
+    GPIO.setup(Motor2A,GPIO.OUT)
+    GPIO.setup(Motor2B,GPIO.OUT)
+    GPIO.setup(Motor2E,GPIO.OUT)
+
+
+
+setup_motor()
+
+
+pygame.init()
+stick=joystick.Joystick(0)
+stick.init()
+
+numbuttons = stick.get_numbuttons()
+print("Initialized " + stick.get_name())
+print("Device has {} number of buttons".format(numbuttons))
+
+interval=0.1
+
+try:
+    done=False
+    while done == False:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # If user clicked close
+                done=True # Flag that we are done so we exit this loop
+            # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
+            if event.type == pygame.JOYBUTTONDOWN:
+                handle_button(event)
+            if event.type == pygame.JOYBUTTONUP:
+                handle_button(event)
+
+except (KeyboardInterrupt, SystemExit):
+    print("Caught kbd interrupt or systemexit")
+    pygame.quit()
+    GPIO.cleanup()
+
+print("All done, exiting")
